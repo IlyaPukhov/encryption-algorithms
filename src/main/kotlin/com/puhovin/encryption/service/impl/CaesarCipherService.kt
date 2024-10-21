0e8b51cb78a3b47e337bbcb0e39c53b0f663ed4f
Ilya Pukhov <pukhovin@answer-42.ru>
2024-10-21 15:15:04 +0300

    Оптимизировал алгоритм для шифра Цезаря

--- src/main/kotlin/com/puhovin/encryption/service/impl/CaesarCipherService.kt
+++ src/main/kotlin/com/puhovin/encryption/service/impl/CaesarCipherService.kt
@@ -6,12 +6,37 @@ import org.springframework.stereotype.Service
 @Service
 class CaesarCipherService : CipherService {
 
+    private val upperAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    private val lowerAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    private val alphabetLength = upperAlphabet.length
+
     override fun encrypt(rawMessage: String, key: Int): String {
-        TODO("Not yet implemented")
+        return processMessage(rawMessage, key)
     }
 
     override fun decrypt(encryptedMessage: String, key: Int): String {
-        TODO("Not yet implemented")
+        return processMessage(encryptedMessage, -key)
+    }
+
+    private fun processMessage(message: String, key: Int): String {
+        val shiftedMessage = StringBuilder()
+
+        for (char in message) {
+            when (char) {
+                in upperAlphabet -> shiftedMessage.append(shiftChar(char, key, upperAlphabet))
+                in lowerAlphabet -> shiftedMessage.append(shiftChar(char, key, lowerAlphabet))
+                else -> shiftedMessage.append(char)
+            }
+        }
+        return shiftedMessage.toString()
+    }
+
+    fun shiftChar(char: Char, key: Int, alphabet: String): Char {
+        val index = alphabet.indexOf(char)
+        return if (index != -1) {
+            val shiftedIndex = (index + key + alphabetLength) % alphabetLength
+            alphabet[shiftedIndex]
+        } else char
     }
 
 }
