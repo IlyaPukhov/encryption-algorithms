525a692e5fc2af59d4a948f46b1471acbb516a05
Ilya Pukhov <pukhovin@answer-42.ru>
2024-10-21 13:01:16 +0300

    Initial commit

---
+++ src/main/kotlin/com/puhovin/encryption/EncryptionAlgorithmsApplication.kt
@@ -0,0 +1,11 @@
+package com.puhovin.encryption
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EncryptionAlgorithmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<EncryptionAlgorithmsApplication>(*args)
+}
---
+++ src/test/kotlin/com/puhovin/encryption/EncryptionAlgorithmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.puhovin.encryption
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EncryptionAlgorithmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
