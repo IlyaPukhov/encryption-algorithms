478447f9438c513c76caf56da3d31084da012ebb
Ilya Pukhov <pukhovin@answer-42.ru>
2024-10-21 14:48:20 +0300

    Оптимизация контроллера

--- src/main/kotlin/com/puhovin/encryption/controller/CaesarCipherController.kt
+++ src/main/kotlin/com/puhovin/encryption/controller/CaesarCipherController.kt
@@ -3,7 +3,9 @@ package com.puhovin.encryption.controller
 import com.puhovin.encryption.dto.CaesarCipherRequest
 import com.puhovin.encryption.service.CaesarCipherService
 import jakarta.validation.Valid
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,14 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/caesar_cipher")
 class CaesarCipherController(private val caesarCipherService: CaesarCipherService) {
 
-    @PostMapping("/encode")
-    fun caesarCipherEncrypt(@Valid request: CaesarCipherRequest): ResponseEntity<String> {
-        return ResponseEntity.ok(caesarCipherService.encrypt(request.message, request.key))
-    }
+    private val logger = LoggerFactory.getLogger(CaesarCipherController::class.java)
 
-    @PostMapping("/decode")
-    fun caesarCipherDecrypt(@Valid request: CaesarCipherRequest): ResponseEntity<String> {
-        return ResponseEntity.ok(caesarCipherService.decrypt(request.message, request.key))
+    @PostMapping("/{action}")
+    fun caesarCipher(
+        @PathVariable action: String,
+        @Valid request: CaesarCipherRequest
+    ): ResponseEntity<String> {
+        logger.info("Received request to $action with message: ${request.message} and key: ${request.key}")
+        return when (action.lowercase()) {
+            "encode" -> ResponseEntity.ok(caesarCipherService.encrypt(request.message, request.key))
+            "decode" -> ResponseEntity.ok(caesarCipherService.decrypt(request.message, request.key))
+            else -> ResponseEntity.badRequest().body("Invalid action: $action")
+        }
     }
-
 }
