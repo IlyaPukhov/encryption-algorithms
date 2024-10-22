acdfe9cbc2e0e0f7d67e0f5571aef119adbe70c6
Ilya Pukhov <pukhovin@answer-42.ru>
2024-10-22 19:03:19 +0300

    Перевесил валидацию на поля, а не на геттеры

--- src/main/kotlin/com/puhovin/encryption/dto/CaesarCipherRequest.kt
+++ src/main/kotlin/com/puhovin/encryption/dto/CaesarCipherRequest.kt
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.NotNull
 import org.hibernate.validator.constraints.Range
 
 data class CaesarCipherRequest(
-    @get:NotNull val message: String,
-    @get:NotNull @get:Range(min = 1, max = 32) val key: Int = 3
+    @field:NotNull val message: String,
+    @field:NotNull @field:Range(min = 1, max = 32) val key: Int = 3
 )
