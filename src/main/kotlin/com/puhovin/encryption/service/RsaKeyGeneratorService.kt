78bb0b4ae6447985cccb393ec79cb65726ee24b8
Ilya Pukhov <pukhovin@answer-42.ru>
2024-10-26 17:35:10 +0300

    fix p and q injection from properties

--- src/main/kotlin/com/puhovin/encryption/service/RsaKeyGeneratorService.kt
+++ src/main/kotlin/com/puhovin/encryption/service/RsaKeyGeneratorService.kt
@@ -2,6 +2,7 @@ package com.puhovin.encryption.service
 
 import com.puhovin.encryption.util.KeyEncoderDecoder
 import com.puhovin.encryption.util.MathUtils
+import com.puhovin.encryption.util.MessageService
 import jakarta.annotation.PostConstruct
 import kotlin.properties.Delegates
 import org.springframework.beans.factory.annotation.Value
@@ -9,14 +10,15 @@ import org.springframework.stereotype.Service
 
 @Service
 class RsaKeyGeneratorService(
-    private val keyEncoder: KeyEncoderDecoder
+    private val keyEncoder: KeyEncoderDecoder,
+    private val messageService: MessageService
 ) {
 
-    @get:Value("\${encrypt.rsa.p}")
-    private var p by Delegates.notNull<Long>()
+    @Value("\${encrypt.rsa.p}")
+    private var p: Long = 0
 
-    @get:Value("\${encrypt.rsa.q}")
-    private var q by Delegates.notNull<Long>()
+    @Value("\${encrypt.rsa.q}")
+    private var q: Long = 0
 
     private var n by Delegates.notNull<Long>()
     private var e by Delegates.notNull<Long>()
@@ -24,48 +26,28 @@ class RsaKeyGeneratorService(
 
     @PostConstruct
     fun init() {
+        if (p == 0L || q == 0L) {
+            throw IllegalStateException(messageService.getMessage("error.rsa-encrypt.pq-is-not-initialized"))
+        }
+
         n = p * q
         val phi = (p - 1) * (q - 1)
         e = findPublicExponent(phi)
         d = calculatePrivateD(e, phi)
     }
 
-    fun getPublicKey(): String = keyEncoder.encodeKey(Pair(e, n))
+    fun getPublicKey() = keyEncoder.encodeKey(Pair(e, n))
 
-    fun getPrivateKey(): String = keyEncoder.encodeKey(Pair(d, n))
+    fun getPrivateKey() = keyEncoder.encodeKey(Pair(d, n))
 
     private fun findPublicExponent(phi: Long): Long {
         var e = 3L
-        while (MathUtils.gcd(e, phi) != 1L && e < n) {
+        while (MathUtils.gcd(e, phi) != 1L) {
             e += 2
         }
         return e
     }
 
-    // Функция для нахождения мультипликативной обратной по модулю
-    private fun calculatePrivateD(e: Long, phi: Long): Long {
-        var m0 = phi
-        var y = 0L
-        var x = 1L
-
-        if (phi == 1L) return 0
-
-        var eTemp = e
-        while (eTemp > 1) {
-            val q = eTemp / m0
-            val t = m0
-
-            m0 = (eTemp % m0)
-            eTemp = t
-
-            val t1 = y
-            y = x - q * y
-            x = t1
-        }
-
-        if (x < 0) x += phi
-
-        return x
-    }
+    private fun calculatePrivateD(e: Long, phi: Long) = MathUtils.modularInverse(e, phi)
 
 }
--- src/main/kotlin/com/puhovin/encryption/util/MathUtils.kt
+++ src/main/kotlin/com/puhovin/encryption/util/MathUtils.kt
@@ -2,6 +2,7 @@ package com.puhovin.encryption.util
 
 object MathUtils {
 
+    // Функция для нахождения НОД
     fun gcd(a: Long, b: Long): Long {
         var x = a
         var y = b
@@ -11,6 +12,32 @@ object MathUtils {
             y = x % y
             x = temp
         }
+        return x
+    }
+
+    // Функция для нахождения мультипликативной обратной по модулю
+    fun modularInverse(e: Long, phi: Long): Long {
+        var m0 = phi
+        var y = 0L
+        var x = 1L
+
+        if (phi == 1L) return 0
+
+        var eTemp = e
+        while (eTemp > 1) {
+            val q = eTemp / m0
+            val t = m0
+
+            m0 = (eTemp % m0)
+            eTemp = t
+
+            val t1 = y
+            y = x - q * y
+            x = t1
+        }
+
+        if (x < 0) x += phi
+
         return x
     }
 
