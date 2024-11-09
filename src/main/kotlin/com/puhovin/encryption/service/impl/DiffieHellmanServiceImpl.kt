1975d9fc742002a0f7d2f06f77c0b10c1855af99
Ilya Pukhov <pukhovin@answer-42.ru>
2024-11-10 00:19:43 +0300

    Задокументировал алгоритм поиска простых чисел Добавил диапазон

--- src/main/kotlin/com/puhovin/encryption/service/impl/DiffieHellmanServiceImpl.kt
+++ src/main/kotlin/com/puhovin/encryption/service/impl/DiffieHellmanServiceImpl.kt
@@ -3,15 +3,35 @@ package com.puhovin.encryption.service.impl
 import com.puhovin.encryption.service.DiffieHellmanService
 import com.puhovin.encryption.util.MathUtils
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
+/**
+ * Реализация протокола Диффи-Хеллмана для выработки общего секретного ключа.
+ *
+ * Сервис предоставляет методы для генерации простых чисел, закрытых и открытых ключей,
+ * а также для вычисления общего секретного ключа.
+ */
 @Service
 class DiffieHellmanServiceImpl : DiffieHellmanService {
 
     private val logger = LoggerFactory.getLogger(DiffieHellmanServiceImpl::class.java)
 
+    @Value("\${encrypt.diffie-hellman.min}")
+    private val minNumber = 0L
+
+    @Value("\${encrypt.diffie-hellman.max}")
+    private val maxNumber = 0L
+
+    /**
+     * Генерирует простое число в диапазоне от [minNumber] до [maxNumber].
+     *
+     * Этот метод генерирует случайное простое число, используя алгоритм проверки простоты.
+     *
+     * @return Сгенерированное простое число.
+     */
     override fun generatePrime(): Long {
-        var candidate = (1L..Long.MAX_VALUE).random() or 1L
+        var candidate = (minNumber..maxNumber).random() or 1L
 
         while (!MathUtils.isPrime(candidate)) {
             candidate += 2
